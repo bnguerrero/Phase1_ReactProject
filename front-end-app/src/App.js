7cee4dc59b15d0646b6cc52e1a78a595a29c59df
brianne guerrero <bnguerrero@cpp.edu>
2024-09-09 02:48:12 -0400

    fixing update section

--- front-end-app/src/App.js
+++ front-end-app/src/App.js
@@ -1,10 +1,18 @@
+import React, {useState} from 'react';
 import './App.css';
 import customer_data from './assets/customer.json';
 import CustomerTable from './components/CustomerTabe';
+import AddCustomer from './components/AddCustomer';
+
 function App() {
+  const[customer, setCustomer] =useState([]);
+  const addCustomer = (newCustomer) => {
+    setCustomer([...CustomElementRegistry, newCustomer]);
+  };
   return (
     <div className='card-container'>
       <CustomerTable data ={customer_data}/>
+      <AddCustomer addCustomer = {addCustomer}/>
     </div>
   );
 }
