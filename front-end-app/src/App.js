672d174833505fd7569abddb8035f750f6fb52aa
bnguerrero <89376675+bnguerrero@users.noreply.github.com>
2024-09-10 14:21:27 -0400

    Update App.js
    edited App.js file

--- front-end-app/src/App.js
+++ front-end-app/src/App.js
@@ -1,18 +1,87 @@
 import React, {useState} from 'react';
 import './App.css';
-import customer_data from './assets/customer.json';
-import CustomerTable from './components/CustomerTabe';
-import AddCustomer from './components/AddCustomer';
 
 function App() {
-  const[customer, setCustomer] =useState([]);
-  const addCustomer = (newCustomer) => {
-    setCustomer([...CustomElementRegistry, newCustomer]);
+ 
+  // Static array of data
+  const dataArray = [
+    { name: 'Jack Jackson', email: 'jackj@abc.com' , password: 'jackj'},
+    { name: 'Katie Kates', email: 'katieK@abc.com', password: 'katiek' },
+    { name: 'Gen Glenns', email: 'gleng@abc.come', password: 'gleng' }
+  ];
+
+  /*these functions will display messages on the console depending 
+  if the button selected is  Delete, Save, or Cancel */
+  const onDeleteClick = () => {
+    console.log('in onDeleteClick()');
   };
+
+  const onSaveClick = () => {
+    console.log('in onSaveClick()');
+  };
+    
+  const onCancelClick = () => {
+    console.log('in onCancelClick()');
+  };
+
   return (
     <div className='card-container'>
-      <CustomerTable data ={customer_data}/>
-      <AddCustomer addCustomer = {addCustomer}/>
+      <div className='customer_table'>
+        <h5>Customer List</h5>
+          <table>
+            <thead>
+              <tr>
+                <th>Name </th>
+                <th>Email </th>
+                <th>Password </th>
+              </tr>
+            </thead>
+            <tbody>
+              {dataArray.map((customer, index) => (
+                <tr key = {index}>
+                  <td>{customer.name}</td>
+                  <td>{customer.email}</td>
+                  <td>{customer.password}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+      </div>
+      <div>
+            <h5> Update</h5>
+            <form className="p-3">
+                <div className="form-group">
+                    <label htmlFor="name">Name: </label>
+                    <input 
+                        type="text"
+                        className="form-control"
+                        id="name"
+                        name = "name"
+                    />
+                </div>
+                <div className="form-group"> 
+                <label htmlFor="email">Email: </label>
+                    <input 
+                        type="text"
+                        className="form-control"
+                        id="email"
+                        name = "email"
+                    />
+                </div>
+                <div className="form-group"> 
+                <label htmlFor="pass">Pass: </label>
+                    <input 
+                        type="text"
+                        className="form-control"
+                        id="pass"
+                        name = "pass"
+                    />
+                </div>
+                <button type="button" onClick={onDeleteClick}>Delete</button>
+                <button type="button" onClick={onSaveClick}>Save</button>
+                <button type="button" onClick={onCancelClick}>Cancel</button>
+            </form>
+        </div>
     </div>
   );
 }
