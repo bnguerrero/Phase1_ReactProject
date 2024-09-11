2eb65841b572f8549d173ddc67dde9445333623a
bnguerrero <89376675+bnguerrero@users.noreply.github.com>
2024-09-11 12:25:14 -0400

    Update App.js
    add list-item selection

--- front-end-app/src/App.js
+++ front-end-app/src/App.js
@@ -1,14 +1,49 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
- 
+
   // Static array of data
   const dataArray = [
-    { name: 'Jack Jackson', email: 'jackj@abc.com' , password: 'jackj'},
+    { name: 'Jack Jackson', email: 'jackj@abc.com', password: 'jackj' },
     { name: 'Katie Kates', email: 'katieK@abc.com', password: 'katiek' },
     { name: 'Gen Glenns', email: 'gleng@abc.come', password: 'gleng' }
   ];
+
+  const [customer, setCustomer] = useState(null);
+  const [customerData, setCustomerData] = useState({
+    name: '',
+    email: '',
+    password: ''
+  });
+
+
+  const onCustomerClick = (selectedCustomer) => {
+    if (customer && customer.name === selectedCustomer.name) {
+      setCustomer(null);
+      setCustomerData({
+        name: '',
+        email: '',
+        password: ''
+      });
+    } else {
+      setCustomer(selectedCustomer);
+      setCustomerData({
+        name: selectedCustomer.name,
+        email: selectedCustomer.email,
+        password: selectedCustomer.password
+      });
+    }
+  };
+
+  // function for handling input changes in the form
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setCustomerData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+  }
 
   /*these functions will display messages on the console depending 
   if the button selected is  Delete, Save, or Cancel */
@@ -19,7 +54,7 @@ function App() {
   const onSaveClick = () => {
     console.log('in onSaveClick()');
   };
-    
+
   const onCancelClick = () => {
     console.log('in onCancelClick()');
   };
@@ -32,59 +67,72 @@ function App() {
     <div className='card-container'>
       <div className='customer_table'>
         <h5>Customer List</h5>
-          <table>
-            <thead>
-              <tr>
-                <th>Name </th>
-                <th>Email </th>
-                <th>Password </th>
+        <table>
+          <thead>
+            <tr>
+              <th>Name </th>
+              <th>Email </th>
+              <th>Password </th>
+            </tr>
+          </thead>
+          <tbody>
+            {dataArray.map((customerItem, index) => (
+              <tr key={index} onClick={() => onCustomerClick(customerItem)}
+              style={{fontWeight: customer && customer.name === customerItem.name ? 'bold': 'normal'}}>
+                <td>{customerItem.name}</td>
+                <td>{customerItem.email}</td>
+                <td>{customerItem.password}</td>
               </tr>
-            </thead>
-            <tbody>
-              {dataArray.map((customer, index) => (
-                <tr key = {index} onClick={() => onCustomer(customer)}>
-                  <td>{customer.name}</td>
-                  <td>{customer.email}</td>
-                  <td>{customer.password}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+            ))}
+          </tbody>
+        </table>
       </div>
       <div className='customer_form'>
         <h5> Update</h5>
         <form className="p-3">
           <div className="form-group">
             <label htmlFor="name">Name: </label>
-            <input 
+            <input
               type="text"
               className="form-control"
               id="name"
-              name = "name"
+              name="name"
+              value={customerData.name}
+              onChange={handleInputChange}
             />
           </div>
-          <div className="form-group"> 
+          <div className="form-group">
             <label htmlFor="email">Email: </label>
-            <input 
+            <input
               type="text"
               className="form-control"
               id="email"
-              name = "email"
+              name="email"
+              value={customerData.email}
+              onChange={handleInputChange}
             />
           </div>
-          <div className="form-group"> 
+          <div className="form-group">
             <label htmlFor="pass">Pass: </label>
-            <input 
+            <input
               type="text"
               className="form-control"
               id="pass"
-              name = "pass"
+              name="pass"
+              value={customerData.password}
+              onChange={handleInputChange}
             />
           </div>
           <div className="buttons-container">
             <button type="button" onClick={onDeleteClick}>Delete</button>
             <button type="button" onClick={onSaveClick}>Save</button>
-            <button type="button" onClick={onCancelClick}>Cancel</button>
+            <button type="button" onClick={() => {
+              setCustomerData({
+                name: '',
+                email: '',
+                password: ''
+              });
+            }} >Cancel</button>
           </div>
         </form>
       </div>
